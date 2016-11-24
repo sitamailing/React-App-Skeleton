316728eee19b90da044a9cfb2104c6eea9c6d92a
Sita Prajapati <sitaprajapati@lftechnology.com>
2016-11-24 16:19:42 +0545

    Added eslint

--- src/client/app/components/app/Layout.js
+++ src/client/app/components/app/Layout.js
@@ -1,12 +1,10 @@
 import React, {Component} from 'react';
-import env from 'dotenv';
 import Header from './Header';
 import Footer from './Footer';
 
-env.load();
-
 class Layout extends Component {
     render() {
+        this.state = {title: 'title'};
         return (
             <div className="container">
                 <Header/>
