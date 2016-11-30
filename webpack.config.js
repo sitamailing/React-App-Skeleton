7932a5c8f83ece803ddf608d5346584476495ee3
Sita Prajapati <sitaprajapati@lftechnology.com>
2016-11-30 12:14:18 +0545

    Added the webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ var config = {
     entry: APP_DIR + '/index.js',
     output: {
         path: BUILD_DIR,
-        filename: 'bundle.js'
+        filename: "bundle.js",
     },
     module: {
         loaders: [
